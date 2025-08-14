929b1f7ab311df83e33e1c3ab6c05c2035b29a8f
Dinesh Reddy Gali <128059225+Dinesh-Reddy10@users.noreply.github.com>
2025-08-15 00:14:58 +0530

    Update DatabaseConnection.java

--- src/DatabaseConnection.java
+++ src/DatabaseConnection.java
@@ -3,11 +3,12 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/school_db?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
+    private static final String URL = "local host jdbc url";
     private static final String USER = "root";
-    private static final String PASSWORD = "Dinesh2945+@";
+    private static final String PASSWORD = "Your Password";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
 }
+
