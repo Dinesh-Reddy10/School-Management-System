8e441a03a78cc348bccdbcc374abacb261c8e116
Dinesh Reddy Gali <128059225+Dinesh-Reddy10@users.noreply.github.com>
2025-08-15 09:58:03 +0530

    Update DatabaseConnection.java

--- src/DatabaseConnection.java
+++ src/DatabaseConnection.java
@@ -4,11 +4,12 @@ import java.sql.SQLException;
 
 public class DatabaseConnection {
     private static final String URL = "local host jdbc url";
-    private static final String USER = "root";
+    private static final String USER = "username";
     private static final String PASSWORD = "Your Password";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
 }
+
 
